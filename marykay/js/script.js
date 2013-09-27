16c944eaeb92fbc2b6ed45064f54bdd82142e801
Alona <trinity2006@gmail.com>
2013-09-27 17:40:39 +0300

    added checkbox to table & added a numeric stepper

--- marykay/js/script.js
+++ marykay/js/script.js
@@ -1,18 +1,19 @@
-/*window.onload = function () {
-    var addProd = document.getElementById('add_prod');
-    var getProduct = document.getElementById('get_product');
-    var close_popup = document.getElementById('close_popup');
-    var close_get = document.getElementById('close_get_product');
-    var open_popup = document.getElementById('open_popup');
-    var open_get_popup = document.getElementById('open_get_popup');
-    var close_upd = document.getElementById('close_update_product');
-    var upd_product = document.getElementById('update_product');
-    var open_update_popup = document.getElementById('open_update_popup');    */
 $(document).ready(function () {
 
     $( "#open_popup" ).button().click(function() {
         $( "#pop-up" ).dialog( "open" );
     });
+
+    //To do somesing...here...
+/*    $('#price').stepper();
+    $('#price').stepper({
+        type: 'float',       // Allow floating point numbers
+        wheel_step:1,       // Wheel increment is 1
+        arrow_step: 0.5,    // Up/Down arrows increment is 0.5
+        limit: [0,0]         // No negative values
+       // onStep: function( val, up )
+      //  {}
+    });     */
 
     $(function() {
         $( "#pop-up" ).dialog({
@@ -22,37 +23,26 @@ $(document).ready(function () {
             modal: true,
             buttons: {
                 "addProd": function addProduct() {
-                 //   $('#addProd').click(function addProduct() {
-                        //     console.log('starting adding new product');
-                        //     var id_prod = $("#inp1").val();
                         var s = {};
                         s.name = $("#name").val();
                         s.price = 1 * $("#price").val();
-                        s.availability = $("#availability").prop("checked");   ///.is(':checked');
+                        s.availability = $("#availability").prop("checked");
                         $.ajax({
                             url: '/marykay/products',
                             type: "POST",
                             data: JSON.stringify(s),
-                            //data: JSON.stringify({"name": "Changed name", "price": 26, "availability": true}),
-                            /*   dataType: "json",*/
                             contentType: "application/json; charset=utf-8",
                             dataType: 'text',
                             success: function () {
                                 alert("success!");
                                 $("#pop-up").dialog( "close" );
-                                /*       close_pop_up('#pop-up');*/
                                 loadProduct();
                             },
                             error: function (xhr, textStatus, textMessage) {
                                 alert("error!");
                                 $("#pop-up").dialog( "close" );
-                                /*        close_pop_up('#pop-up');*/
                             }
                         });
-                        //   $("#prod td").each(function(){$(this).parent().remove()});
-                        //   loadProduct();
-
-                  //  });
                 },
                 Cancel: function() {
                     $( this ).dialog( "close" );
@@ -73,7 +63,6 @@ $(document).ready(function () {
             modal: true,
             buttons: {
                 "Find": function getSpecificProduct() {
-                //    $('#getProduct').click(function getSpecificProduct() {
                     var id = $("#id_prod").val();
                     $.ajax({
                         type: "GET",
@@ -84,18 +73,18 @@ $(document).ready(function () {
                                 if (k != "version") {
                                     if (k == "availability") {
                                         alert(v);
-                                        $("#find_rez").append("availability: " + v + ", <br>");
+                                        $(".find_rez").append("availability: " + v + ", <br>");
                                     }
                                     else if (k == "id") {
-                                        $("#find_rez").append("id: " + v + ", <br>");
+                                        $(".find_rez").append("id: " + v + ", <br>");
                                         alert(v);
                                     }
                                     else if (k == "name") {
-                                        $("#find_rez").append("name: " + v + ", <br>");
+                                        $(".find_rez").append("name: " + v + ", <br>");
                                         alert(v);
                                     }
                                     else if (k == "price") {
-                                        $("#find_rez").append("price: " + v + ", <br>");
+                                        $(".find_rez").append("price: " + v + ", <br>");
                                         alert(v);
                                     }
                                 }
@@ -113,37 +102,7 @@ $(document).ready(function () {
         });
     });
 
-
-
- /*   close_popup.onclick = function closePopup() {
-        close_pop_up('#pop-up');
-    }
-    open_popup.onclick = function openPopup() {
-        open_pop_up('#pop-up');
-        //$("#pop-up table > tr").append($("<tr><td colspan=2><input id='add_prod' type='button' value='Добавить'/><input id='close_popup' type='button' value='Закрыть'/></td></tr>"));
-    }
-
-    open_get_popup.onclick = function openGetPopup() {
-        open_pop_up('#get-pop-up');
-    }
-
-    close_get.onclick = function closeGetProduct() {
-        close_pop_up('#get-pop-up');
-    }             */
-
-    //open_update_popup.onclick = function openUpdatePopup(){
-    //    open_pop_up('#update-pop-up');
-    // }
-
- /*   close_upd.onclick = function closeUpdateProduct() {
-        close_pop_up('#update-pop-up');
-    }              */
-
-
-
 });
-
-/*};*/
 
 $(document).ready(function () {
     loadProduct();
@@ -170,7 +129,8 @@ var onLoadProductResult = function (data) {
 
     $.each(data, function (index, value) {
         tbody.append('<tr>');
-        tbody.append('<td>' + value.availability + '</td>');
+        tbody.append('<td><input type="checkbox" class="update_availability" disabled="disabled" id="t_upd_availability_' + value.id + '" /></td>');
+        $("#t_upd_availability_" + value.id ).prop('checked', value.availability);
         tbody.append('<td>' + value.id + '</td>');
         tbody.append('<td>' + value.name + '</td>');
         tbody.append('<td>' + value.price + '</td>');
@@ -193,9 +153,11 @@ var onLoadProductResult = function (data) {
         text: false
     });
 
+  //  $('.update_availability').click(onUpdateAvailability);
     $('.editButton').click(onEditButtonClick);
     $('.deletetButton').click(onDeleteButtonClick);
 };
+
 
 $(function() {
     $( "#update-pop-up" ).dialog({
@@ -205,7 +167,6 @@ $(function() {
         modal: true,
         buttons: {
             "Update": function () {
-                //$( this ).dialog( "close" );
                 var product =  $(this).data("product");
                 onEditComplete(product);
                 $( this ).dialog( "close" );
@@ -218,8 +179,6 @@ $(function() {
 });
 
 var onEditButtonClick = function () {
-    //console.log(this.id);
-
     var productId = this.id.replace("product_", "");
     var product;
     for (var i = 0; i < products.length; i++) {
@@ -230,49 +189,31 @@ var onEditButtonClick = function () {
     }
 
     if (product){
-       // console.log(product.price);
-       //product.price*=2;
-
-        // show popup
         $( "#update-pop-up" ).data("product", product).dialog( "open" );
-
-   /*     open_pop_up('#update-pop-up');*/
-        // set data
-        $("#upd_availability").val(product.availability);
-        //$("#upd_id_prod").val(product.id);
+        $("#upd_availability").prop('checked', product.availability);
         $("#upd_name").val(product.name);
         $("#upd_price").val(product.price);
-        // onEditComplete(
-     /*  $('#update_product').click(function (){
-            onEditComplete(product);
-        });       */
-        // close popup
-        //updateProduct(product);
     }
 };
 
 var onEditComplete = function (product) {
-    console.log(this.id);
     var upd_product = {};
     upd_product.name = $("#upd_name").val();
     upd_product.price = 1 * $("#upd_price").val();
-    upd_product.availability = !!$("#upd_availability").val();
+    upd_product.availability = $("#upd_availability").prop('checked');
     upd_product.id = product.id;
     upd_product.version = product.version;
     updateProduct(upd_product);
 
 
 };
-//upd_product.onclick =
-//var updateProduct = function (product) {
+
 var updateProduct = function (upd_product) {
-    console.log(upd_product.id);
     $.ajax({
         type: "PUT",
         url: "/marykay/products/" + upd_product.id,
 
         data: JSON.stringify(upd_product),
-        /*   dataType: "json",*/
         contentType: "application/json; charset=utf-8",
         dataType: 'text',
         success: function () {
@@ -300,39 +241,10 @@ var deleteProduct = function(productId) {
         url: "/marykay/products/" + productId,
         dataType: "text",
         success: function () {
-            //  alert("success!");
             loadProduct();
         },
         error: function () {
-            //   alert("error!");
         }
     });
 
-    //$("#prod").find("tbody td").each(function(){$(this).remove()});
-    //$("#prod").find("tbody tr").each(function(){$(this).remove()});
-    //$("#prod > tbody  > tr").each(function(){$(this).remove()});
-
-
 };
-
-
-/*function open_pop_up(box) {
-    $("#overlay").show();
-    $(box).center_pop_up();
-    $(box).show(500);
-}
-
-function close_pop_up(box) {
-    $(box).hide(500);
-    $("#overlay").delay(550).hide(1);
-}
-
-$(document).ready(function () {
-
-    $.fn.center_pop_up = function () {
-        this.css('position', 'absolute');
-        this.css('top', ($(window).height() - this.height()) / 2 + $(window).scrollTop() + 'px');
-        this.css('left', ($(window).width() - this.width()) / 2 + $(window).scrollLeft() + 'px');
-    }
-
-});  */
