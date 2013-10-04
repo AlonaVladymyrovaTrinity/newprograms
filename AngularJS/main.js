d792545eac1cc8c26407f43803d158a6e03603ff
Alona <trinity2006@gmail.com>
2013-10-04 16:21:51 +0300

    vidio 4 sharing-data-between-controllers

--- AngularJS/main.js
+++ AngularJS/main.js
@@ -1,6 +1,15 @@
 /**
  * Created by Trinity on 04.10.13.
  */
-function FirstCtrl($scope){
-    $scope.data = {message: "Hello"};
+var myApp = angular.module("myApp",[]);
+
+myApp.factory("Data", function() {
+    return {message: "I'm data"}
+})
+
+function FirstCtrl($scope,Data){
+    $scope.data=Data;
+}
+function SecondCtrl($scope,Data){
+    $scope.data=Data;
 }
